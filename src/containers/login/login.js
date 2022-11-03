18ec4d5249d4c173d88c9aef231f63e96571d506
Esakiammal35 <105477583+Esakiammal35@users.noreply.github.com>
2022-11-03 13:30:52 +0530

    update login changes

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,13 +1,112 @@
-import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
+// hooks
+import useResponsive from '../../hooks/useResponsive';
+// components
+import Logo from '../../assets/images/kotakLogo (2).png';
+import Iconify from '../../components/iconify';
+import HomeImage from '../../assets/images/illustration_login (4).png'
+// sections
+import { LoginForm } from '../../sections/auth/login';
 
-class Login extends React.Component
-{
+// ----------------------------------------------------------------------
 
-    render(){
-        return <>
-            <h1>Login</h1>
-        </>
-    }
+const StyledRoot = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex',
+  },
+}));
+
+const StyledSection = styled('div')(({ theme }) => ({
+  width: '100%',
+  maxWidth: 480,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  boxShadow: theme.customShadows.card,
+  backgroundColor: theme.palette.background.default,
+}));
+
+const StyledContent = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(12, 0),
+}));
+
+// ----------------------------------------------------------------------
+
+export default function LoginPage() {
+  const mdUp = useResponsive('up', 'md');
+
+  return (
+    <>
+      {/* <Helmet>
+        <title> Login | Minimal UI </title>
+      </Helmet> */}
+
+      <StyledRoot>
+        {/* <Logo
+          sx={{
+            position: 'fixed',
+            top: { xs: 16, sm: 24, md: 40 },
+            left: { xs: 16, sm: 24, md: 40 },
+          }}
+        /> */}
+
+        
+
+        {mdUp && (
+          <StyledSection >
+            
+            <Typography variant="h3" sx={{ px: 5, mt: 2, mb: 10 }}>
+              Hi, Welcome Back
+            </Typography>
+            <img src={HomeImage} alt="login" />
+          </StyledSection>
+        )}
+
+        <Container maxWidth="sm">
+          <StyledContent>
+          <img src={Logo} style={{width:"30%", marginLeft:"34%", paddingBottom:"15%"}}></img>
+            <Typography variant="h4" sx={{mb:2}} gutterBottom>
+            Sign in to Drishti
+            </Typography>
+
+            {/* <Typography variant="body2" sx={{ mb: 5 }}>
+              Don’t have an account? {''}
+              <Link variant="subtitle2">Get started</Link>
+            </Typography> */}
+
+            {/* <Stack direction="row" spacing={2}>
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
+              </Button>
+
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
+              </Button>
+
+              <Button fullWidth size="large" color="inherit" variant="outlined">
+                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
+              </Button>
+            </Stack> */}
+
+            {/* <Divider sx={{ my: 3 }}>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                OR
+              </Typography>
+            </Divider> */}
+
+            <LoginForm />
+          </StyledContent>
+        </Container>
+      </StyledRoot>
+    </>
+  );
 }
-
-export default Login;
